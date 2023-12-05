1e895ffe4f49f164739dd226f9e9b0b7ae90c449
sji8562 <whaqlzkem@gmail.com>
2023-12-05 13:35:43 +0900

    mnguserupdate

--- src/main/java/com/tenco/toyproject/controller/MngController.java
+++ src/main/java/com/tenco/toyproject/controller/MngController.java
@@ -6,13 +6,18 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.tenco.toyproject._core.utils.ApiUtils;
+import com.tenco.toyproject.dto.MngUserDTO;
 import com.tenco.toyproject.repository.entity.User;
 import com.tenco.toyproject.service.MngService;
 import com.tenco.toyproject.vo.PageVO;
@@ -31,17 +36,13 @@ public class MngController {
 	public String mngLogin() {
 		return "mng/login";
 	}
-	@GetMapping("table")
-	public String table() {
-		return "mng/table-basic";
-	}
 
 	@GetMapping("/product/list")
 	public String productList() {
 		return "mng/product/list";
 	}
 
-	@GetMapping("user")
+	@GetMapping("/user")
 	public String UserTable(Model model, PageVO pageVO, @RequestParam(value="nowPage", required=false)String nowPage
             , @RequestParam(value="cntPerPage", required=false)String cntPerPage) {
 		
@@ -72,6 +73,19 @@ public class MngController {
 	        List<User> userList = mngService.findAll(pageVO);
 	        model.addAttribute("userList", userList);
 		return "mng/user-table";
+	}
+	@GetMapping("/user/{id}/update")
+	public String userUpdate(@PathVariable(value = "id")Integer id , Model model) {
+		User user = mngService.findById(id);
+		model.addAttribute("user",user);
+		System.out.println(user.getUserName()+"님을 불러왔습니다.");
+		return "mng/user/update";
+	}
+	@PostMapping("/user/{id}/update")
+	public String  userUpdated(@PathVariable(value = "id")Integer id ,MngUserDTO.UpdateDTO updateDTO) {
+		mngService.update(id,updateDTO);
+//		ResponseEntity.ok().body(ApiUtils.success(null))
+		return "redirect:/mng/user";
 	}
 
 	@GetMapping("/product/detail/{pIdx}")
